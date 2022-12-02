0a2bb412775bfe44f8b146020bb017007ed3d824
Jacob Clark <jacobcs@zoho.com>
2022-12-02 01:31:33 -0800

    feat: added MUI

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,15 @@
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export default function Home() {
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography>asdf asdf asd</Typography>
+    </Container>
+  );
+}
